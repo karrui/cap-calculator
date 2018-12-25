3ebd8e2e21a00225c8b48c4690f5a65b97bdbc9f
Lau Kar Rui <karrui.lau@gmail.com>
2018-12-25 23:43:01 +0800

    refactor: remove semesterCap from CapHeader

--- src/components/CapHeader.tsx
+++ src/components/CapHeader.tsx
@@ -3,14 +3,10 @@ import { RootState } from "src/store/configureStore";
 import { connect } from "react-redux";
 import { Dispatch, Action } from "redux";
 import { addSemester, removeSemester } from "src/actions/misc";
-import { ICapCalcState } from "src/reducers/capCalculator";
 
 interface ICapHeaderProps {
   totalMcs: number;
   totalGradePoint: number;
-  fullSemesterMcs: ICapCalcState["semesterMcs"];
-  fullSemesterGradePoint: ICapCalcState["semesterGradePoint"];
-  currSem: string;
 
   onAddSemester: (event: React.MouseEvent<HTMLButtonElement>) => void;
   handleRemoveSemester: (event: React.MouseEvent<HTMLButtonElement>) => void;
@@ -18,11 +14,8 @@ interface ICapHeaderProps {
 
 const mapStateToProps = (state: RootState) => ({
   numSemesters: state.misc.numSemesters,
-  currSem: state.misc.currSemester,
   totalMcs: state.capCalculator.totalMcs,
   totalGradePoint: state.capCalculator.totalGradePoint,
-  fullSemesterMcs: state.capCalculator.semesterMcs,
-  fullSemesterGradePoint: state.capCalculator.semesterGradePoint,
 });
 
 const mapDispatchToProps = (dispatch: Dispatch) => ({
@@ -31,48 +24,28 @@ const mapDispatchToProps = (dispatch: Dispatch) => ({
 });
 
 const mergeProps = (
-  {
-    numSemesters,
-    currSem,
-    totalMcs,
-    totalGradePoint,
-    fullSemesterMcs,
-    fullSemesterGradePoint,
-  }: any,
+  { numSemesters, totalMcs, totalGradePoint }: any,
   { onAddSemester, onRemoveSemester }: any
 ) => ({
-  currSem,
   totalMcs,
   totalGradePoint,
-  fullSemesterGradePoint,
-  fullSemesterMcs,
   onAddSemester,
-  handleRemoveSemester: (event: React.MouseEvent<HTMLButtonElement>) => {
+  handleRemoveSemester: () => {
     onRemoveSemester(numSemesters);
   },
 });
 
 const CapHeader: React.FunctionComponent<ICapHeaderProps> = ({
-  fullSemesterGradePoint,
-  fullSemesterMcs,
   totalGradePoint,
   totalMcs,
   onAddSemester,
   handleRemoveSemester,
-  currSem,
 }) => {
   return (
     <div className="cap-header">
       <div className="total-cap">
         CAP: {totalGradePoint ? totalGradePoint / totalMcs : "--"}
       </div>
-      {fullSemesterGradePoint[currSem] && fullSemesterMcs[currSem] ? (
-        <div className="sem-cap">
-          Semester CAP:{" "}
-          {fullSemesterGradePoint[currSem] / fullSemesterMcs[currSem]}
-        </div>
-      ) : null}
-
       <button onClick={onAddSemester}>Add Semester</button>
       <button onClick={handleRemoveSemester}>Remove Semester</button>
     </div>
