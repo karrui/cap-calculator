ed5cb9b30bdf4d6cebe5a0eb974c684e54956a92
Lau Kar Rui <karrui.lau@gmail.com>
2018-12-25 23:43:59 +0800

    feat: display all semester’s SavedTable in a single page

--- src/components/SavedTable.tsx
+++ src/components/SavedTable.tsx
@@ -4,6 +4,7 @@ import { connect } from "react-redux";
 import { RootState } from "../store/configureStore";
 import { ISavedModuleState } from "src/reducers/savedModules";
 import Module from "./Module";
+import { ICapCalcState } from "src/reducers/capCalculator";
 
 const SavedTableHeader: React.FunctionComponent = () => (
   <thead>
@@ -17,27 +18,52 @@ const SavedTableHeader: React.FunctionComponent = () => (
 );
 
 const mapStateToProps = (state: RootState) => ({
-  savedModules: state.savedModules[state.misc.currSemester],
+  numSemesters: state.misc.numSemesters,
+  savedModules: state.savedModules,
+  semesterGradePoint: state.capCalculator.semesterGradePoint,
+  semesterMcs: state.capCalculator.semesterMcs,
 });
 
 interface ISavedTableProps {
   savedModules: ISavedModuleState;
+  numSemesters: number;
+  semesterGradePoint: ICapCalcState["semesterGradePoint"];
+  semesterMcs: ICapCalcState["semesterMcs"];
 }
 
 const SavedTable: React.FunctionComponent<ISavedTableProps> = ({
   savedModules,
+  numSemesters,
+  semesterGradePoint,
+  semesterMcs,
 }) => {
-  return (
-    <table className="table">
-      <SavedTableHeader />
-      <tbody>
-        {savedModules &&
-          Object.keys(savedModules).map(moduleCode => (
-            <Module key={moduleCode} module={savedModules[moduleCode]} />
-          ))}
-      </tbody>
-    </table>
-  );
+  const savedSemesterModules = [];
+  for (let i = numSemesters; i > 0; i = i - 1) {
+    savedSemesterModules.push(
+      <div className="sem-table" key={i}>
+        Semester {i}
+        <div className="sem-cap">
+          Semester CAP:{" "}
+          {semesterMcs[i] ? semesterGradePoint[i] / semesterMcs[i] : "--"}
+        </div>
+        <table className="table">
+          <SavedTableHeader />
+          <tbody>
+            {savedModules[i] &&
+              Object.keys(savedModules[i]).map(moduleCode => (
+                <Module
+                  key={moduleCode}
+                  semester={i}
+                  module={savedModules[i][moduleCode]}
+                />
+              ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+
+  return <div className="saved-tables">{savedSemesterModules}</div>;
 };
 
 export default connect(mapStateToProps)(SavedTable);
