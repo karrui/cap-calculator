5509fd894fae7cf80b88f40c2d5eadc28f3b62b8
Lau Kar Rui <karrui.lau@gmail.com>
2018-12-30 21:06:48 +0900

    chore: remove redux-logger middleware in production

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -21,11 +21,13 @@ const __REDUX_DEVTOOLS_EXTENSION_COMPOSE__ =
 const composeEnhancers =
   (window[__REDUX_DEVTOOLS_EXTENSION_COMPOSE__] as typeof compose) || compose;
 
+const middleware =
+  process.env.NODE_ENV !== "production"
+    ? applyMiddleware(reduxThunk, reduxLogger)
+    : applyMiddleware(reduxThunk);
+
 export default () => {
-  const store = createStore(
-    persistedReducer,
-    composeEnhancers(applyMiddleware(reduxThunk, reduxLogger))
-  );
+  const store = createStore(persistedReducer, composeEnhancers(middleware));
   const persistor = persistStore(store);
   return { store, persistor };
 };
