3fd9286bf67a1242da43d7380c10ee2a55bf3d74
Lau Kar Rui <karrui.lau@gmail.com>
2019-03-28 12:57:59 -0500

    feat: add style and scroll to currentSem element

--- src/components/SavedTable.tsx
+++ src/components/SavedTable.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import * as ReactDOM from "react-dom";
 import { connect } from "react-redux";
 
 import { RootState } from "../store/configureStore";
@@ -19,6 +20,7 @@ const SavedTableHeader: React.FunctionComponent = () => (
 
 const mapStateToProps = (state: RootState) => ({
   numSemesters: state.misc.numSemesters,
+  currSemester: state.misc.currSemester,
   savedModules: state.savedModules,
   semesterGradePoint: state.capCalculator.semesterGradePoint,
   semesterMcs: state.capCalculator.semesterMcs,
@@ -27,46 +29,122 @@ const mapStateToProps = (state: RootState) => ({
 interface ISavedTableProps {
   savedModules: ISavedModuleState;
   numSemesters: number;
+  currSemester: string;
   semesterGradePoint: ICapCalcState["semesterGradePoint"];
   semesterMcs: ICapCalcState["semesterMcs"];
 }
 
-const SavedTable: React.FunctionComponent<ISavedTableProps> = ({
-  savedModules,
-  numSemesters,
-  semesterGradePoint,
-  semesterMcs,
-}) => {
-  const savedSemesterModules = [];
-  for (let i = numSemesters; i > 0; i = i - 1) {
-    savedSemesterModules.push(
-      <div className="sem-table" key={i}>
-        <div className="sem-header-details">
-          <div className="sem-info">
-            <div>Semester {i}</div>
-            <div className="sem-cap">
-              {semesterMcs[i]
-                ? `SAP: ${(semesterGradePoint[i] / semesterMcs[i]).toFixed(2)}`
-                : ""}
-            </div>
-          </div>
-          <SavedTableHeader />
-        </div>
-        <div className="sem-table-wrapper">
-          {savedModules[i] &&
-            Object.keys(savedModules[i]).map(moduleCode => (
-              <Module
-                key={moduleCode}
-                semester={i}
-                module={savedModules[i][moduleCode]}
-              />
-            ))}
-        </div>
-      </div>
-    );
+class SavedTable extends React.Component<ISavedTableProps, {}> {
+  constructor(props: ISavedTableProps) {
+    super(props);
   }
 
-  return <div className="saved-tables">{savedSemesterModules}</div>;
-};
+  public render() {
+    const {
+      savedModules,
+      numSemesters,
+      semesterGradePoint,
+      semesterMcs,
+      currSemester,
+    } = this.props;
+
+    const savedSemesterModules: JSX.Element[] = [];
+    for (let i = numSemesters; i > 0; i = i - 1) {
+      const isCurrSem = i.toString() === currSemester;
+      savedSemesterModules.push(
+        <div
+          className="saved-table-wrapper"
+          key={i}
+          ref={isCurrSem ? "currSemRef" : ""}
+        >
+          <div className={`saved-table ${isCurrSem ? "curr-sem" : ""}`}>
+            <div className="sem-header-details">
+              <div className="sem-info">
+                <div>Semester {i}</div>
+                <div className="sem-cap">
+                  {semesterMcs[i]
+                    ? `SAP: ${(semesterGradePoint[i] / semesterMcs[i]).toFixed(
+                        2
+                      )}`
+                    : ""}
+                </div>
+              </div>
+              <SavedTableHeader />
+            </div>
+            <div className="sem-table-wrapper" />
+            {savedModules[i] &&
+              Object.keys(savedModules[i]).map(moduleCode => (
+                <Module
+                  key={moduleCode}
+                  semester={i}
+                  module={savedModules[i][moduleCode]}
+                />
+              ))}
+          </div>
+        </div>
+      );
+    }
+
+    return <div className="saved-tables">{savedSemesterModules}</div>;
+  }
+
+  componentDidUpdate(prevProps: ISavedTableProps) {
+    if (this.props.currSemester !== prevProps.currSemester) {
+      this.scrollToSemester();
+    }
+  }
+
+  private scrollToSemester() {
+    const itemComponent = this.refs.currSemRef;
+    if (itemComponent) {
+      const domNode = ReactDOM.findDOMNode(itemComponent);
+      if (domNode instanceof Element) {
+        domNode.scrollIntoView({
+          behavior: "smooth",
+          block: "start",
+          inline: "nearest",
+        });
+      }
+    }
+  }
+}
+
+// const SavedTable: React.FunctionComponent<ISavedTableProps> = ({
+//   savedModules,
+//   numSemesters,
+//   semesterGradePoint,
+//   semesterMcs,
+// }) => {
+//   const savedSemesterModules: JSX.Element[] = [];
+//   for (let i = numSemesters; i > 0; i = i - 1) {
+//     savedSemesterModules.push(
+//       <div className="sem-table" key={i}>
+//         <div className="sem-header-details">
+//           <div className="sem-info">
+//             <div>Semester {i}</div>
+//             <div className="sem-cap">
+//               {semesterMcs[i]
+//                 ? `SAP: ${(semesterGradePoint[i] / semesterMcs[i]).toFixed(2)}`
+//                 : ""}
+//             </div>
+//           </div>
+//           <SavedTableHeader />
+//         </div>
+//         <div className="sem-table-wrapper">
+//           {savedModules[i] &&
+//             Object.keys(savedModules[i]).map(moduleCode => (
+//               <Module
+//                 key={moduleCode}
+//                 semester={i}
+//                 module={savedModules[i][moduleCode]}
+//               />
+//             ))}
+//         </div>
+//       </div>
+//     );
+//   }
+
+//   return <div className="saved-tables">{savedSemesterModules}</div>;
+// };
 
 export default connect(mapStateToProps)(SavedTable);
