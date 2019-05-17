967506105a00e762edfd13cf1c8fff81b632aaa8
Lau Kar Rui <karrui.lau@gmail.com>
2019-05-16 19:34:12 -0500

    feat: add toggle dark/light theme redux

--- src/reducers/misc.ts
+++ src/reducers/misc.ts
@@ -4,16 +4,22 @@ import {
   ADD_SEMESTER,
   REMOVE_SEMESTER,
   SET_NUM_SEMESTER,
+  LIGHT_MODE,
+  Theme,
+  TOGGLE_THEME,
+  DARK_MODE,
 } from "./constants";
 
 const defaultMiscState: IMiscState = {
   currSemester: "1",
   numSemesters: 1,
+  theme: LIGHT_MODE,
 };
 
 interface IMiscState {
   currSemester: string;
   numSemesters: number;
+  theme: Theme;
 }
 
 const miscReducer = (state = defaultMiscState, action: IMiscAction) => {
@@ -51,6 +57,12 @@ const miscReducer = (state = defaultMiscState, action: IMiscAction) => {
         ...state,
         currSemester: newCurrSemester,
         numSemesters: state.numSemesters - 1,
+      };
+    }
+    case TOGGLE_THEME: {
+      return {
+        ...state,
+        theme: state.theme === LIGHT_MODE ? DARK_MODE : LIGHT_MODE,
       };
     }
     default:
