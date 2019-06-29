e3e68bbaf13e8c47fed97ff51cf046ab9d92e10e
Lau Kar Rui <karrui.lau@gmail.com>
2019-06-30 03:53:02 +0800

    fix: stop modal state in store from persisting

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,15 +1,23 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import misc from "./misc";
 import moduleBank from "./moduleBank";
 import savedModules from "./savedModules";
 import capCalculator from "./capCalculator";
 
+const miscPersistConfig = {
+  storage,
+  key: "misc",
+  blacklist: ["customModuleModalInfo"],
+};
+
 const rootReducer = combineReducers({
-  misc,
   moduleBank,
   savedModules,
   capCalculator,
+  misc: persistReducer(miscPersistConfig, misc),
 });
 
 export default rootReducer;
